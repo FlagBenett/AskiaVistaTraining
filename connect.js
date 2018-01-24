a8c5f687f51abfea27a5c7657daa202cc67e9f36
StephenPadawan <stephen@askia.com>
2018-01-24 19:50:44 +0100

    added connection file

---
+++ connect.js
@@ -0,0 +1,7 @@
+askiaVista.config({
+        baseUrl: "https://[Your domain]/AskiaVistaReader.Net4/",
+        survey: "[]",
+        authenticityToken:"[]"
+        //locale:"fr"
+        //languageUrlParam : 'surveyLang'
+});
